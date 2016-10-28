f500665634a24240cb384045b38a7064cbd11683
Quynh Nguyen <qnguyen@linagora.com>
2016-10-28 12:50:20 +0700

    JAMES-1842: Add more binding for MessageId

--- dockerfiles/compilation/java-6/compile.sh
+++ dockerfiles/compilation/java-6/compile.sh
@@ -44,9 +44,9 @@ git checkout $SHA1
 # Compilation
 
 if [ "$SKIPTESTS" = "skipTests" ]; then
-   mvn package -DskipTests -Pjpa
+   mvn package -DskipTests -Pjpa,with-assembly
 else
-   mvn package -Pjpa
+   mvn package -Pjpa,with-assembly
 fi
 
 # Retrieve result
