0dacc43fededfdf6d6644acdfd40c8b50eb9cc4c
quynhn <qnguyen@linagora.com>
2017-10-11 16:17:37 +0700

    JAMES-2182 Add MPT testing to evaluate the share mailbox in IMAP

--- mpt/core/src/main/java/org/apache/james/mpt/script/ImapScriptedTestProtocol.java
+++ mpt/core/src/main/java/org/apache/james/mpt/script/ImapScriptedTestProtocol.java
@@ -19,6 +19,7 @@
 
 package org.apache.james.mpt.script;
 
+import org.apache.james.mailbox.model.MailboxACL;
 import org.apache.james.mailbox.model.MailboxPath;
 import org.apache.james.mpt.api.ImapHostSystem;
 
@@ -37,7 +38,25 @@ public class ImapScriptedTestProtocol extends GenericSimpleScriptedTestProtocol<
             system.createMailbox(mailboxPath);
         }
     }
-    
+
+    private static class CreateRights implements PrepareCommand<ImapHostSystem> {
+
+        final MailboxPath mailboxPath;
+        final String userName;
+        final MailboxACL.Rfc4314Rights rights;
+
+        public CreateRights(MailboxPath mailboxPath, String userName, MailboxACL.Rfc4314Rights rights) {
+            this.mailboxPath = mailboxPath;
+            this.userName = userName;
+            this.rights = rights;
+        }
+
+        public void prepare(ImapHostSystem system) throws Exception {
+            system.grantRights(mailboxPath, userName, rights);
+        }
+    }
+
+
     public ImapScriptedTestProtocol(String scriptDirectory, ImapHostSystem hostSystem) throws Exception {
         super(scriptDirectory, hostSystem);
     }
@@ -46,4 +65,7 @@ public class ImapScriptedTestProtocol extends GenericSimpleScriptedTestProtocol<
         return withPreparedCommand(new CreateMailbox(mailboxPath));
     }
 
+    public ImapScriptedTestProtocol withRights(MailboxPath mailboxPath, String userName, MailboxACL.Rfc4314Rights rights) {
+        return withPreparedCommand(new CreateRights(mailboxPath, userName, rights));
+    }
 }
