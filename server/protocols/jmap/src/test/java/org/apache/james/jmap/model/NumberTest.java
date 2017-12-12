1ce87507f407588b6edf72ee04d60fb9da3f2b53
quynhn <qnguyen@linagora.com>
2017-12-12 13:49:44 +0700

    JAMES-2255 JMAP supports Number model which present for all number data from JMAP layer

---
+++ server/protocols/jmap/src/test/java/org/apache/james/jmap/model/NumberTest.java
@@ -0,0 +1,97 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.Test;
+
+import com.google.common.math.LongMath;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+public class NumberTest {
+    @Test
+    public void fromIntShouldReturnMinValueWhenNegativeValueWithLenient() throws Exception {
+        assertThat(Number.lenientFactory().fromInt(-1).asInt())
+            .isEqualTo(0);
+    }
+
+    @Test
+    public void fromLongShouldReturnMinValueWhenNegativeValueWithLenient() throws Exception {
+        assertThat(Number.lenientFactory().fromLong(-1).asLong())
+            .isEqualTo(0);
+    }
+
+    @Test
+    public void fromIntShouldThrowWhenNegativeValueWithStrict() throws Exception {
+        assertThatThrownBy(() ->
+            Number.strictFactory().fromInt(-1))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void fromIntShouldReturnIntegerValue() throws Exception {
+        assertThat(Number.strictFactory().fromInt(1).asLong())
+            .isEqualTo(1);
+    }
+
+    @Test
+    public void fromLongShouldThrowWhenNegativeValue() throws Exception {
+        assertThatThrownBy(() ->
+            Number.strictFactory().fromLong(-1))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void fromLongShouldThrowWhenOver2Pow53Value() throws Exception {
+        assertThatThrownBy(() ->
+            Number.strictFactory().fromLong(Number.MAX_LONG_VALUE + 1))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void fromLongShouldReturnLongValue() throws Exception {
+        assertThat(Number.strictFactory().fromLong(1).asLong())
+            .isEqualTo(1);
+    }
+
+    @Test
+    public void ensureLessThanShouldThrowWhenOverSpecifiedValue() throws Exception {
+        assertThatThrownBy(() ->
+            Number.strictFactory().fromInt(11).ensureLessThan(10))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void ensureLessThanShouldReturnNumberWhenEqualValue() throws Exception {
+        Number number = Number.strictFactory().fromInt(10);
+        assertThat(number.ensureLessThan(10))
+            .isEqualTo(number);
+    }
+
+    @Test
+    public void ensureLessThanShouldReturnNumberWhenLessThanMaxValue() throws Exception {
+        Number number = Number.strictFactory().fromInt(10);
+        assertThat(number.ensureLessThan(11))
+            .isEqualTo(number);
+    }
+}
